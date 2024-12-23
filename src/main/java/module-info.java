92816e5bb546a3f74c88be674221c2c62e41cec2
Ardenel <arthur.aquilano@gmail.com>
2024-12-23 12:00:15 +0100

    Update pom and module-info to manage import error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,13 @@ module com.projet.da.projet_da {
 
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
     requires org.hibernate.orm.core;
     requires java.naming;
     requires java.sql;
     requires java.persistence;
     requires jbcrypt;
+    requires java.desktop;
 
     opens com.projet.da50.projet_da50.model to org.hibernate.orm.core;
     opens com.projet.da50.projet_da50.view to javafx.graphics;
